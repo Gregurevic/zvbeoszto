29e9d7e46e33360bb46381c3b3bb9578ad146d98
Gregurevic <paul.greg.indy@gmail.com>
2020-09-26 21:05:44 +0200

    boostrap 4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-import "bootstrap"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,3 +15,5 @@ import "bootstrap"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "bootstrap";
+import "./src/application.scss";
